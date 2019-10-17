06f8d8ec710f4042922ca5790accc0b257f04e38
Gabriel Borralho <39606678+gabrielrhcp@users.noreply.github.com>
2019-10-16 23:08:28 -0300

    Update qst04.lua

--- Variáveis/qst04.lua
+++ Variáveis/qst04.lua
@@ -1,3 +1,4 @@
+print("Digite um numero de no maximo 4 digitos")
 num = io.read("*n")
 unid, dez, cent, mil = 0,0,0,0
 
@@ -9,4 +10,5 @@ num,cent = math.floor(num/10), tostring(num%10)
 --print(cent)
 num,mil = math.floor(num/10), tostring(num%10)
 --print(mil)
+print("Invertendo fica")
 print(unid .. dez .. cent .. mil)
